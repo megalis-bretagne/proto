3c496e9e838635951d577cf23c593ff925d1b651
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2022-08-17 13:53:03 +0000

    Delete home component

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { DeliberationsComponent } from './components/deliberations.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { DashboardComponent } from './components/dashboard.component';
@@ -22,11 +21,6 @@ const routes: Routes = [
 {
   path: 'autres-actes',
   component: ActesAutresComponent,
-  canActivate: [AuthGuard]
-},
-{
-  path: 'home',
-  component: HomeComponent,
   canActivate: [AuthGuard]
 }
 ,{
